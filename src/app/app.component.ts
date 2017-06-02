c5e76d0385fc90fe598bb9e70ed40d8124ce6403
PC-022\PC-022 <thanhxuanhd007@gmail.com>
2017-06-02 18:53:16 +0700

    Main Add Script

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, AfterContentChecked, ElementRef, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements AfterContentChecked {
+  /**
+   *
+   */
+  constructor(private elementRef: ElementRef) {
+  }
+  ngAfterContentChecked() {
+    var m = document.createElement("script");
+    m.type = "text/javascript";
+    m.src = "../assets/js//material.min.js";
+    this.elementRef.nativeElement.appendChild(m);
+    
+    var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.src = "../assets/js/material-dashboard.js";
+    this.elementRef.nativeElement.appendChild(s);
+  }
 }
